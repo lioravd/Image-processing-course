7e92cb1b21c9de8d7da5088d256ddf02c0225ac0
lioravd <113434611+lioravd@users.noreply.github.com>
2023-04-12 20:22:18 +0300

    Delete Hw1 directory

--- Hw1/LA-206087611_AR-314077033/adjust_contrast.m
+++
@@ -1,4 +0,0 @@
-%Lior Avadyayev_206087611 Ayalla Reuven_314077033
-function [contimg]=adjust_contrast(img,range_low,range_high)    %1.4  Contrast
-contimg=((range_high-range_low).*img)+range_low;
-end
--- Hw1/LA-206087611_AR-314077033/plots.m
+++
@@ -1,4 +0,0 @@
-%Lior Avadyayev_206087611 Ayalla Reuven_314077033
-function [] = plots(img)  %short for plots
-plot(1:800,img(400,:));
-end
